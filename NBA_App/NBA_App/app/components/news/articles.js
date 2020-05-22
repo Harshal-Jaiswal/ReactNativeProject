b06a507f024520712574c6c04571c83c91deb823
Harshal.jaiswal <harshaljaiswal25@gmail.com>
2020-05-22 20:09:49 +0530

    updated

--- NBA_App/NBA_App/app/components/news/articles.js
+++ NBA_App/NBA_App/app/components/news/articles.js
@@ -4,38 +4,84 @@ import {
   StyleSheet,
   View,
   Text,
+  StatusBar,
+  Image,
 } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Moment from 'moment';
+import LogoTitle from '../../utils/logo';
+
 
 class ArticleComponent extends Component {
-  static navigationOptions ={
-    title: 'usr',
-    
-    headerStyle:{
-        backgroundColor:'#f4511e'
+  static navigationOptions = {
+
+    headerTitleAlign: 'center',
+    headerTitle: () => (<LogoTitle />),
+    headerStyle: {
+      backgroundColor: '#001338'
     },
-    headerTintColor:'blue',
-    headerTitleStyle:{
-        fontWeight:'bold',
-        color:'green'
-    }
-}
+    headerTintColor: 'blue',
+  }
 
   render() {
    
+    const params =this.props.navigation.state.params;
+
     return (
-      <View style={styles.container}>
-        <Text>
-        Article Component
-        </Text>
+      <View >
+        <StatusBar
+          backgroundColor='#001338'
+          barStyle='light-content'
+        />
+        <ScrollView style={{ backgroundColor: '#f0f0f0' }}>
+          <Image 
+            style={{height:250}}
+            source ={{uri:params.image}}
+            resizeMode ="cover"
+          />
+
+          <View style={styles.articleContainer}>
+            <Text style={styles.articleTitle}>
+              {params.title}
+            </Text>
+            <Text style={styles.articleData}>
+              {params.team} - Posted at {Moment(params.date).format('d MMMM')}
+            </Text>
+          </View>
+          <View style={styles.articleContent}>
+            <Text style ={styles.articleText}>
+              {params.content}
+            </Text>
+          </View>
+        </ScrollView >
       </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
+  articleContainer: {
+    padding:10
+  },
+  articleTitle:{
+    fontSize:23,
+    color:'#323232',
+    fontFamily:'Roboto-Bold'
+  },
+  articleData:{
+    fontSize:12,
+    color:'#828282', 
+    fontFamily:'Roboto-Light'
+  },
+  articleContent:{
+    marginTop:30,
+
+  },
+  articleText:{
+    fontSize:14,
+    color:'#828282',
+    lineHeight:20,
+    fontFamily:'Roboto-Light'
   }
 });
 
